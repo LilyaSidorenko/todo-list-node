7df1994e733153d7998091fbd2d8570467230bcc
Liliia_Sydorenko <lilya.sidorenko1@gmail.com>
2017-05-05 17:54:00 +0300

    Create todo list

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url' : 'mongodb://lilya.sidorenko1:Testing88@ds137090.mlab.com:37090/todo-list'
+};
---
+++ public/home-tasks.js
@@ -0,0 +1,48 @@
+var update = document.getElementsByClassName("done")
+for (var j = 0; j < update.length; j++) {
+
+    update[j].addEventListener("click", function () {
+        fetch("/home-tasks/tasks", {
+            method: "put",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "task": this.nextElementSibling.innerHTML,
+                "classDone": "class-done"
+
+            })
+        })
+            .then(res => {
+                if (res.ok) return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                window.location.reload(true)
+            })
+    });
+}
+var del = document.getElementsByClassName("delete");
+
+for (var i = 0; i < del.length; i++) {
+    del[i].addEventListener("click", function () {
+        fetch("/home-tasks/tasks", {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "task": this.previousElementSibling.innerHTML
+            })
+
+        })
+            .then(res => {
+                if (res.ok) return res.json()
+            })
+            .then(data => {
+                console.log(data);
+                window.location.reload(true)
+            })
+    })
+}
+
+
+
